edf83a7947fe82383a70346f1378095f4261fd81
Austin Gilbert <ceretullis@gmail.com>
2012-07-16 19:05:17 -0500

    Renamed Message.h -> MessageBase.h Templatized MessageBase on ProcessingFunctor type so that users of the framework can declare their own ProcessingFunctor with all possible Message types defined in client code. Removed wield::ProcessingFunctor as it is no longer needed.

---
+++ MessageBase.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <atomic>
+#include <boost/intrusive_ptr.hpp>
+
+namespace wield {
+
+    template<class ProcessingFunctor>
+	class MessageBase
+	{
+	public:
+        typedef boost::intrusive_ptr<MessageBase> smartptr;
+        MessageBase()
+            : reference_count_(0)
+        {
+        }
+
+        virtual ~MessageBase(){}
+
+		virtual void ProcessWith(ProcessingFunctor& process)
+        {
+            process(*this);
+        }
+
+    private:
+        std::atomic<size_t> reference_count_;
+
+        template<class ProcessingFunctor>
+        friend void intrusive_ptr_add_ref(MessageBase<ProcessingFunctor>* m);
+
+        template<class ProcessingFunctor>
+        friend void intrusive_ptr_release(MessageBase<ProcessingFunctor>* m);
+    };
+
+    template<class ProcessingFunctor>
+    void intrusive_ptr_add_ref(MessageBase<ProcessingFunctor>* m)
+    {
+        ++m->reference_count_;
+    }
+
+    template<class ProcessingFunctor>
+    void intrusive_ptr_release(MessageBase<ProcessingFunctor>* m)
+    {
+        if( 0 == --m->reference_count_)
+        {
+            delete m;
+        }
+    }
+}
--- unit_test/testProcessingFunctor-UT.cpp
+++ unit_test/testProcessingFunctor-UT.cpp
@@ -1,25 +1,64 @@
 #include <UnitTest++/UnitTest++.h>
-#include <wield/Message.h>
-#include <wield/ProcessingFunctor.h>
+#include <wield/MessageBase.h>
 
 namespace {
 
 	using namespace wield;
 	
-	class TestProcessingFunctor : public ProcessingFunctor
-	{
-	public:
-		TestProcessingFunctor()
-			: messageFunctorCalled(false)
-			, testMessageFunctorCalled(false)
-		{
-		}
-		
-		virtual void operator()(Message& msg) { messageFunctorCalled = true; }
-		
-		bool messageFunctorCalled;
-		bool testMessageFunctorCalled;
-	};
+    class TestMessage;
+    class TestMessage2;
+
+    class ProcessingFunctor;
+    typedef MessageBase<ProcessingFunctor> Message;
+
+    class ProcessingFunctor
+    {
+    public:
+        ProcessingFunctor()
+        {
+        }
+
+        virtual void operator()(Message& msg) = 0;
+        virtual void operator()(TestMessage& msg) = 0;
+        virtual void operator()(TestMessage2& msg) = 0;
+    };
+    
+    class TestProcessingFunctor : public ProcessingFunctor
+    {
+    public:
+        TestProcessingFunctor()
+            : message1Called(false)
+            , message2Called(false)
+        {
+        }
+
+        virtual void operator()(Message& msg){}
+        virtual void operator()(TestMessage& msg){ message1Called = true; }
+        virtual void operator()(TestMessage2& msg) { message2Called = true;}
+
+        bool message1Called;
+        bool message2Called;
+    };
+
+    class TestMessage : public MessageBase<ProcessingFunctor>
+    {
+    public:
+        virtual void ProcessWith(ProcessingFunctor& process) override
+        {
+            process(*this);
+        }
+    };
+
+    class TestMessage2 : public MessageBase<ProcessingFunctor>
+    {
+    public:
+        virtual void ProcessWith(ProcessingFunctor& process) override
+        {
+            process(*this);
+        }
+    };
+
+
 	
 	
 	TEST(verifyProcessingFunctorInitialization)
@@ -30,10 +69,20 @@ namespace {
 	TEST(verifyProcessingFunctorIsCalledWhenProcessingMessage)
 	{
 		TestProcessingFunctor tpf;
-		Message::smartptr m = new Message();
+		Message::smartptr m = new TestMessage();
 		
 		m->ProcessWith(tpf);
-		CHECK(tpf.messageFunctorCalled);
-		CHECK(! tpf.testMessageFunctorCalled);
+		CHECK(tpf.message1Called);
+		CHECK(! tpf.message2Called);
 	}
+
+    TEST(verifyProcessingFunctorIsCalledWhenProcessingMessage2)
+    {
+        TestProcessingFunctor tpf;
+        Message::smartptr m = new TestMessage2();
+
+        m->ProcessWith(tpf);
+        CHECK(!tpf.message1Called);
+        CHECK(tpf.message2Called);
+    }
 }
