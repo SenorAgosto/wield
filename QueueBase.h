0c6ca272406d58509f86ab09b28db9a999d68f95
Austin Gilbert <ceretullis@gmail.com>
2012-07-22 14:29:06 -0500

    Added QueueBase templated on the ProcessingFunctor and queue implementation.

---
+++ QueueBase.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <wield/MessageBase.h>
+
+namespace wield {
+
+    template<class ProcessingFunctor, class queue_type>
+    class QueueBase
+    {
+    public:
+        typedef typename queue_type::size_type size_type;
+
+        QueueBase()
+        {
+        }
+
+        void push(const typename MessageBase<ProcessingFunctor>::smartptr& value)
+        {
+            queue_.push(value);
+        }
+
+        bool try_pop(typename MessageBase<ProcessingFunctor>::smartptr& value)
+        {
+            if(!queue_.empty())
+            {
+                value = queue_.front();
+                queue_.pop();
+
+                return true;
+            }
+
+            return false;
+        }
+        
+        size_type unsafe_size(void) const
+        {
+            return queue_.size();
+        }
+
+    private:
+        //queue_type<typename MessageBase<ProcessingFunctor>::smartptr, std::allocator<typename MessageBase<ProcessingFunctor> > > queue_;
+        queue_type queue_;
+    };
+}
