98dc12fd3e44f40166c425b5e985cf359de9112c
Austin Gilbert <ceretullis@gmail.com>
2012-08-12 16:21:49 -0500

    Removing unnecessary header.

--- unit_test/TestStage.h
+++ unit_test/TestStage.h
@@ -3,7 +3,6 @@
 
 #include "TestProcessingFunctor.h"
 #include "TestQueue.h"
-#include "TestDispatcher.h"
 #include "Stages.h"
 
 typedef wield::StageBase<Stages, ProcessingFunctor, TestQueue, wield::DispatcherBase> TestStage;
