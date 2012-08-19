277c120a339bfda44ffee91190983980f899e414
ceretullis <austin.gilbert@gmail.com>
2012-08-19 16:10:55 -0500

    Added some classes for logging info/warning/error messages Added a class for redirecting std::clog to an std::ostream object.

---
+++ logging/ScopedClogRedirector.h
@@ -0,0 +1,88 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace wield { namespace logging {
+
+    //! example scoped_clog_redirect<std::stringstream> for capturing logging in a std::stringstream
+    template<typename OStreamType>
+    class ScopedClogRedirector
+    {
+    public:
+        ScopedClogRedirector()
+            : ostream_()
+            , old_clog_(std::clog.rdbuf(ostream_.rdbuf()))
+        {
+        }
+
+        ~ScopedClogRedirector()
+        {
+            // restore the old logger
+            std::clog.rdbuf(old_clog_);
+        }
+
+    private:
+        OStreamType ostream_;
+        std::streambuf* old_clog_;
+    };
+
+    //! specialization for using std::ostringstream
+    template<>
+    class ScopedClogRedirector<std::ostringstream>
+    {
+    public:
+        ScopedClogRedirector()
+            : ostream_()
+            , old_clog_(std::clog.rdbuf(ostream_.rdbuf()))
+        {
+        }
+
+        ~ScopedClogRedirector()
+        {
+            // restore the old logger
+            std::clog.rdbuf(old_clog_);
+        }
+
+        std::string str(void) const
+        {
+            return ostream_.str();
+        }
+
+    private:
+        std::ostringstream ostream_;
+        std::streambuf* old_clog_;
+    };
+
+
+    // specialization for redirecting std::clog to a log file.
+    template<>
+    class ScopedClogRedirector<std::ofstream>
+    {
+    public:
+        ScopedClogRedirector(const std::string& logPath)
+            : logPath_(logPath)
+            , file_stream_(logPath, std::ios_base::out | std::ios_base::app | std::ios_base::ate)
+            , old_clog_(std::clog.rdbuf(file_stream_.rdbuf()))
+        {
+        }
+
+        ~ScopedClogRedirector()
+        {
+            // restore the original clog
+            std::clog.rdbuf(old_clog_);
+        }
+
+        const std::string& logFile(void) const
+        {
+            return logPath_;
+        }
+
+    private:
+        std::string logPath_;
+        std::ofstream file_stream_;
+        std::streambuf* old_clog_;
+    }; 
+
+}}
+
---
+++ unit_test/testLogging-UT.cpp
@@ -0,0 +1,29 @@
+#include <UnitTest++/UnitTest++.h>
+#include <wield/logging/Logging.h>
+#include <wield/logging/ScopedClogRedirector.h>
+
+#include <iostream>
+#include <sstream>
+
+namespace {
+
+    struct LogToStringStreamFixture
+    {
+        wield::logging::ScopedClogRedirector<std::ostringstream> log;
+    };
+
+    TEST_FIXTURE(LogToStringStreamFixture, verifyInfoMessage)
+    {
+        std::clog << wield::logging::InfoMessage("Test Info Message");
+    }
+
+    TEST_FIXTURE(LogToStringStreamFixture, verifyWarningMessage)
+    {
+        std::clog << wield::logging::WarningMessage("Test Warning Message");
+    }
+
+    TEST_FIXTURE(LogToStringStreamFixture, verifyErrorMessage)
+    {
+        std::clog << wield::logging::WarningMessage("Test Error Message");
+    }
+}
