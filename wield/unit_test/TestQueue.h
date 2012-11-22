6f829443188eeb758ae9e82e1ff88a5bfcb97439
ceretullis <austin.gilbert@gmail.com>
2012-11-21 18:36:07 -0600

    Added a header in unit_test to wrap the inclusion of concurrent_queue.h in order to suppress some warnings.

--- wield/unit_test/TestQueue.h
+++ wield/unit_test/TestQueue.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <wield/MessageBase.h>
 #include <wield/QueueBase.h>
-#include <concurrent_queue.h>
 
+#include "ConcurrentQueue.h"
 #include "TestProcessingFunctor.h"
 
 typedef wield::QueueBase<ProcessingFunctor, Concurrency::concurrent_queue<Message::smartptr> > TestQueue;
