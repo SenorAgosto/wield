66677b90ed2eb97fd99bdb79be6502f65d35b6a7
Austin Gilbert <ceretullis@gmail.com>
2013-11-13 21:33:36 -0600

    Use Intel Thread Build Block concurrent_queue implementation on unix.

--- wield/unit_test/platform/ConcurrentQueue.h
+++ wield/unit_test/platform/ConcurrentQueue.h
@@ -1,8 +1,15 @@
 #pragma once
 
 #ifdef _WIN32
-#pragma warning(push)
-#pragma warning(disable : 4127 4625 4626)
-#include <concurrent_queue.h>
-#pragma warning(pop)
+
+    // Use Microsoft's concurrent_queue implementation
+    // Disable some warnings
+    #pragma warning(push)
+    #pragma warning(disable : 4127 4625 4626)
+    #include <concurrent_queue.h>
+    #pragma warning(pop)
+
+#else
+    // Use Intel Thread Build Blocks concurrent_queue
+    #include <tbb/concurrent_queue.h>
 #endif
