6479ac238ca249f4b2efb4b5627a6391d29c9f3c
Austin Gilbert <ceretullis@gmail.com>
2013-11-13 21:44:42 -0600

    Add namespace Concurrency for unix build to match namespace on windows.

--- wield/unit_test/platform/ConcurrentQueue.h
+++ wield/unit_test/platform/ConcurrentQueue.h
@@ -8,8 +8,8 @@
     #pragma warning(disable : 4127 4625 4626)
     #include <concurrent_queue.h>
     #pragma warning(pop)
-
 #else
     // Use Intel Thread Build Blocks concurrent_queue
     #include <tbb/concurrent_queue.h>
+    namespace Concurrency = tbb::strict_ppl;
 #endif
