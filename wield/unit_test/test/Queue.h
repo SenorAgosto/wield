f2a6764417b6833d264272254f9d0128cbc67040
Austin Gilbert <ceretullis@gmail.com>
2013-11-13 21:10:28 -0600

    Correct include path in test example program.

--- wield/unit_test/test/Queue.h
+++ wield/unit_test/test/Queue.h
@@ -2,7 +2,7 @@
 #include <wield/MessageBase.h>
 #include <wield/QueueBase.h>
 
-#include "platform/ConcurrentQueue.h"
+#include "../platform/ConcurrentQueue.h"
 #include "ProcessingFunctor.h"
 
 typedef wield::QueueBase<ProcessingFunctor, Concurrency::concurrent_queue<Message::smartptr> > TestQueue;
