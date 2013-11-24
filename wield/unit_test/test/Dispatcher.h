1923503bd829ee7cb46d07e61db8806bc827da47
Austin Gilbert <ceretullis@gmail.com>
2013-11-24 00:17:31 -0600

    Removing headers that will be replaced by Traits class.

--- wield/unit_test/test/Dispatcher.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <wield/DispatcherBase.h>
-
-#include "Stages.h"
-#include "Stage.h"
-
-namespace test {
-    using Dispatcher = wield::DispatcherBase<Stages, Stage>;
-}
--- wield/unit_test/test/Queue.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <wield/MessageBase.h>
-
-#include "../platform/ConcurrentQueue.h"
-#include "ProcessingFunctor.h"
-
-namespace test {
-    using Queue = Concurrency::concurrent_queue<Message::smartptr>;
-}
