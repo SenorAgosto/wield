cfa7b67d8b1df7cd5b097e787b2f40646bc0222a
Austin Gilbert <ceretullis@gmail.com>
2014-09-27 16:08:58 -0500

    Adding some documentation to DispatcherBase and StageBase.

--- wield/StageBase.h
+++ wield/StageBase.h
@@ -3,6 +3,15 @@
 
 namespace wield {
 
+    /* Stage
+       @StageEnum the enum defining names of all stages.
+       @ProcessingFunctor this stage's processing function.
+       @QueueType the type of queue this stage will get inputs from.
+       @DispatcherType the type of the dispatcher the stage will use to dispatch messages to other stages.
+     
+       A stage is the basic unit of processing in a SEDA architecture. It consists of a queue, and a function
+       to apply to messages in the queue. 
+    */
     template<typename StageEnum, class ProcessingFunctor, class QueueType, template<typename StageEnum2, typename Stage> class DispatcherType>
     class StageBase final
     {
@@ -22,11 +31,19 @@ namespace wield {
         {
         }
 
+        // Insert a message onto the stage's queue
+        // @m the message to insert
         void push(const typename MessageType::smartptr& m)
         {
             queue_.push(m);
         }
 
+        /* process a message
+           
+           pump the queue, if there is a message, process it.
+           
+           @return true if a message was processed, false otherwise.
+        */
         bool process(void)
         {
             typename MessageType::smartptr m;
@@ -39,6 +56,7 @@ namespace wield {
             return false;
         }
 
+        // get the stage's name
         inline StageEnum name(void) const
         {
             return stageName_;
