f924506dca2fdbd46cbedb588395ef1293ffd247
Austin Gilbert <ceretullis@gmail.com>
2014-09-27 15:46:15 -0500

    Adding cldoc documentation, first cut. Not much is documented.

---
+++ _docs/cldoc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export SOURCES=`find wield/ -name *.cpp -or -name *.c | grep -v unit_test`
+export HEADERS=`find wield/ -name *.hpp -or -name *.h | grep -v unit_test`
+
+export CXXOPTS="--std=c++11 -I. -I/Volumes/austirg/homebrew/include"
+
+# HTML documentation
+cldoc generate $CXXOPTS --  --report --type html --output _docs --merge . --language c++ --basedir wield $SOURCES $HEADERS 
