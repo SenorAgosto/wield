b7ea337b3f6178e55ca8f340959bd957aec1dcbb
Austin Gilbert <ceretullis@gmail.com>
2014-10-30 14:22:19 -0500

    Implemented an example of Event Source Stages. A stage that when visited produces and processes an event. In a real application the event data may be coming from a socket read, or user input, etc. The application implementor has the choice of processing data in the stage where the event was created or simply routing the event in the processing functor for the stage. Which is most efficient is likely application specific.

---
+++ wield/unit_test/exampleEventSourceQueue-UT.cpp
@@ -0,0 +1,77 @@
+#include "platform/UnitTestSupport.h"
+
+#include "test_adapter/Traits.h"
+#include "test_adapter/ProcessingFunctor.h"
+#include "test_adapter/Message.h"
+
+#include <wield/adapters/polymorphic/QueueAdapter.h>
+
+namespace {
+    
+    using namespace test_adapter;
+    
+    using Dispatcher = Traits::Dispatcher;
+    using Queue = Traits::Queue;
+    using Stage = Traits::Stage;
+    using Message = Traits::Message;
+    using MessagePtr = typename Message::smartptr;
+    
+    struct EventSourceQueueImpl
+    {
+        void push(const MessagePtr& message)
+        {
+            // here we do nothing. You *could* have source queues
+            // implement actual queues also so they could recieve
+            // message from other stages in addition to creating
+            // events.
+        }
+        
+        // create an event here
+        bool try_pop(MessagePtr& message)
+        {
+            message = new TestMessage();
+            return true;
+        }
+        
+        std::size_t unsafe_size(void) const
+        {
+            return 0;
+        }
+    };
+    
+    // note: use queue adaptor, input stages have different implementations, regular
+    // processing stages continue to use actual queues, etc.
+    struct EventSourceQueue : public wield::adapters::polymorphic::QueueAdapter<ProcessingFunctorInterface, EventSourceQueueImpl>
+    {
+    };
+    
+    // This example demonstrates how events can be created when a
+    // thread visits a stage. This is the preferred way to do source
+    // events, e.g. select/reading from a socket creating an event with
+    // the buffer (or going ahead and parsing the data here then
+    // forwarding the message/event on.
+    TEST(exampleVisitingAStageGeneratesAnEvent)
+    {
+        Dispatcher d;
+        EventSourceQueue q;
+        ProcessingFunctor f;    // you would do the routing in f, or parsing/processing/forwarding in f.
+        
+        Stage s(Stages::Stage1, d, q, f);
+        
+        CHECK_EQUAL(0, f.messageBaseCallCount_);
+        CHECK_EQUAL(0, f.message1CallCount_);
+        CHECK_EQUAL(0, f.message2CallCount_);
+        
+        s.process();
+        
+        CHECK_EQUAL(0, f.messageBaseCallCount_);
+        CHECK_EQUAL(1, f.message1CallCount_);
+        CHECK_EQUAL(0, f.message2CallCount_);
+        
+        s.process();
+        
+        CHECK_EQUAL(0, f.messageBaseCallCount_);
+        CHECK_EQUAL(2, f.message1CallCount_);
+        CHECK_EQUAL(0, f.message2CallCount_);
+    }
+}
