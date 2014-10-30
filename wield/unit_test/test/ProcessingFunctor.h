dc8cc2aeadcabe21a5f6ab478ed97db258078eed
Austin Gilbert <ceretullis@gmail.com>
2014-10-30 12:09:04 -0500

    Changing scoping to protected so classes inheriting from this can get at the member variables.

--- wield/unit_test/test/ProcessingFunctor.h
+++ wield/unit_test/test/ProcessingFunctor.h
@@ -74,24 +74,30 @@ namespace test {
             : messageBaseCalled_(false)
             , message1Called_(false)
             , message2Called_(false)
+            , messageBaseCallCount_(0)
+            , message1CallCount_(0)
+            , message2CallCount_(0)
             , dispatcher_(dispatcher)
         {
         }
 
         void operator()(Message& msg) override
         {
+            messageBaseCallCount_++;
             messageBaseCalled_ = true;
             dispatcher_.dispatch(Stages::Stage2, msg);   //forward the message to stage2
         }
 
         void operator()(TestMessage& msg) override
-        { 
+        {
+            message1CallCount_++;
             message1Called_ = true;
             dispatcher_.dispatch(Stages::Stage2, msg);
         }
 
         void operator()(TestMessage2& msg) override
-        { 
+        {
+            message2CallCount_++;
             message2Called_ = true;
             dispatcher_.dispatch(Stages::Stage2, msg);
         }
@@ -100,11 +106,15 @@ namespace test {
         bool message1Called_;
         bool message2Called_;
 
+        std::size_t messageBaseCallCount_;
+        std::size_t message1CallCount_;
+        std::size_t message2CallCount_;
+        
     private:
         ProcessingFunctorWithDispatcher(const ProcessingFunctorWithDispatcher&) = delete;
         ProcessingFunctorWithDispatcher& operator=(const ProcessingFunctorWithDispatcher&) = delete;
 
-    private:
+    protected:
         Dispatcher& dispatcher_;
     };
 
