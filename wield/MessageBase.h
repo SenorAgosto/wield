e0f8a2eaf549495948c741a370c5f3142afcce21
Austin Gilbert <ceretullis@gmail.com>
2014-11-14 19:41:02 -0600

    Refactoring MessageBase.
    - removed boost::intrusive_ptr from MessageBase, moved to a mixin class.
    - inhanced the functionality of the reference counting. Supports
      threaded and single-threaded reference count fields. Supports a
      static debugging member to assist in unit testing containers
      utilizing boost::intrusive_ptr types.

--- wield/MessageBase.h
+++ wield/MessageBase.h
@@ -1,61 +1,25 @@
 #pragma once
-#include <wield/platform/intrusive_ptr.hpp>
-
-#include <atomic>
-#include <cstddef>
+#include <wield/UsingIntrusiveIn.hpp>
 
 namespace wield {
 
-    /// @note [ARG]: strictly speaking, we don't need to parameterize
-    /// Messages on the ProcessingFunctor. We could introduce an
-    /// interface and force client code ProcessingFunctors to inherit
-    /// from the interface, then use the interface for processWith() here.
-    ///
-    /// However, the current solution allows an interesting potential
-    /// for a message's processWith to invoke methods on the ProcessingFunctor
-    /// prior dispatching into it for processing.
-    ///
-    /// This flexability maybe useful for updating statistics or informing
-    /// the scheduler of events via ProcessingFunctor - if that is how
-    /// client code decides to feed statistics or information into their scheduler.
+    // @note we parameterize on ProcessingFunctor so that we can support
+    // double dispatch against ProcessingFunctors when not all message types
+    // can be known by the wield toolkit.
+    //
+    // However, this also allows the interesting potential for a message's
+    // processWith method to invoke non-virtual methods on the ProcessingFunctor
+    // prior to dispatching into it for proecssing.
+    //
+    // This flexability maybe useful for updating statistics or informing
+    // the scheduler of events via ProcessingFunctor - if that is how
+    // client code decides to feed statistics or information into their scheduler.
     template<class ProcessingFunctor>
-	class MessageBase
+    class MessageBase : public UsingIntrusiveIn<MessageBase<ProcessingFunctor>>
 	{
 	public:
-        typedef boost::intrusive_ptr<MessageBase> smartptr;
         virtual ~MessageBase(){}
-
 		virtual void processWith(ProcessingFunctor& process) = 0;
-
-    protected:
-        MessageBase()
-            : reference_count_(0)
-        {
-        }
-
-        MessageBase(const MessageBase&)
-            : reference_count_(0)
-        { 
-        }
-
-        // assignment operator is default implementation
-        MessageBase& operator=(const MessageBase&){}
-
-    private:
-        friend inline void intrusive_ptr_add_ref(MessageBase<ProcessingFunctor>* m)
-        {
-            ++m->reference_count_;
-        }
-
-        friend inline void intrusive_ptr_release(MessageBase<ProcessingFunctor>* m)
-        {
-            if( 0 == --m->reference_count_)
-            {
-                delete m;
-            }
-        }
-
-    private:
-        std::atomic<std::size_t> reference_count_;
     };
 }
+
