40ee25e56c97283edd16428aa8bc0ca914cdeab9
Austin Gilbert <ceretullis@gmail.com>
2015-03-28 15:16:05 -0500

    Initial commit of Color- scheduling policy.
        This scheduler implements the Color scheduler without
        a queue; it simulates a priority queue using monotonically
        increasing event counters for each stage. When a thread
        requests the next stage to visit, it looks estimates the
        queue depths at each stage and returns the stage with
        deepest queue depth which has not exceeded its concurrency
        maximum.

---
+++ wield/unit_test/test_color_minus/Message.cpp
@@ -0,0 +1,23 @@
+#include "./Message.hpp"
+#include "./ProcessingFunctor.hpp"
+
+namespace test_color_minus {
+    
+    //virtual
+    void TestMessage::processWith(ProcessingFunctorInterface& process)
+    {
+        return process(*this);
+    }
+
+    //virtual
+    void TestMessage2::processWith(ProcessingFunctorInterface& process)
+    {
+        return process(*this);
+    }
+
+    //virtual
+    void TestMessage3::processWith(ProcessingFunctorInterface& process)
+    {
+        return process(*this);
+    }
+}
