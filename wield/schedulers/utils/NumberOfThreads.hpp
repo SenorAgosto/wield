7bbd1c8051a338fe1d1c625d5b074c298234cdcb
Austin Gilbert <ceretullis@gmail.com>
2015-03-28 13:18:32 -0500

    Extracting responsibilities out of Color scheduler into ThreadAssignments class for reuse.

---
+++ wield/schedulers/utils/NumberOfThreads.hpp
@@ -0,0 +1,13 @@
+#pragma once 
+#include <cstddef>
+
+namespace wield { namespace schedulers { namespace utils {
+
+    // This function returns the "idle" number of threads to run
+    // an application with. It chooses the smaller of hardware
+    // cores and number of stages, but correctly handles errors
+    // in the number of reported cores.
+    //
+    // @numberOfStages is the number of stages in the stage graph.
+    std::size_t numberOfThreads(const std::size_t numberOfStages);
+}}}
