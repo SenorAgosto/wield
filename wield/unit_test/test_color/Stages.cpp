e0f673e62dcd29a7cc54e1e670e0074bf139045c
Austin Gilbert <ceretullis@gmail.com>
2015-03-27 19:13:53 -0500

    Needed to add code especially for testing Color scheduler, because I need a non-default dispatcher.

---
+++ wield/unit_test/test_color/Stages.cpp
@@ -0,0 +1,34 @@
+#include "./Stages.hpp"
+
+namespace test_color {
+
+    namespace {
+
+        class LookupStages
+        {
+        public:
+            LookupStages(){}
+
+            std::string operator[](Stages s) const
+            {
+                switch(s)
+                {
+                case Stages::Stage1:          return "Stages::Stage1";
+                case Stages::Stage2:          return "Stages::Stage2";
+                case Stages::Stage3:          return "Stages::Stage3";
+                case Stages::NumberOfEntries: return "Stages::NumberOfEntries";
+                default: break;
+                };
+
+                return "Unknown Value";
+            }
+
+        } static const lookup;
+    }
+
+    std::ostream& operator<<(std::ostream& os, test_color::Stages s)
+    {
+        os << test_color::lookup[s];
+        return os;
+    }
+}
