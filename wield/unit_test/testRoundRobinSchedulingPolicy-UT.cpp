9001b85a23b688b0d2896b1f287340852366fbb3
Austin Gilbert <ceretullis@gmail.com>
2015-04-11 14:45:27 -0500

    Implemented Single-threaded Round-Robin stage scheduling strategy.

---
+++ wield/unit_test/testRoundRobinSchedulingPolicy-UT.cpp
@@ -0,0 +1,43 @@
+#include "./platform/UnitTestSupport.hpp"
+#include <wield/schedulers/RoundRobin.hpp>
+#include <wield/polling_policies/ExhaustivePolling.hpp>
+
+#include "./test/Stages.hpp"
+#include "./test/Traits.hpp"
+#include "./test/ProcessingFunctor.hpp"
+
+namespace {
+
+    struct RoundRobinFixture
+    {
+        RoundRobinFixture()
+            : schedulingPolicy(dispatcher)
+        {
+        }
+
+        using Dispatcher = test::Traits::Dispatcher;
+        using Stage = test::Traits::Stage;
+        using SchedulingPolicy = wield::schedulers::RoundRobin<test::Stages, Dispatcher, Stage, wield::polling_policies::ExhaustivePollingPolicy<test::Stages>>;
+
+        Dispatcher dispatcher;
+        SchedulingPolicy schedulingPolicy;
+    };
+
+    TEST_FIXTURE(RoundRobinFixture, verifyInstantiationOfRoundRobin)
+    {
+    }
+
+    TEST_FIXTURE(RoundRobinFixture, verifyRoundRobinOrdering)
+    {
+        CHECK_EQUAL(test::Stages::Stage1, schedulingPolicy.nextStage(0));
+        CHECK_EQUAL(test::Stages::Stage2, schedulingPolicy.nextStage(0));
+        CHECK_EQUAL(test::Stages::Stage3, schedulingPolicy.nextStage(0));
+        CHECK_EQUAL(test::Stages::Stage1, schedulingPolicy.nextStage(0));
+    }
+
+    TEST_FIXTURE(RoundRobinFixture, verifyCanBeUsedInSchedulerBase)
+    {
+        using Scheduler = wield::SchedulerBase<SchedulingPolicy>;
+        Scheduler scheduler(dispatcher);
+    }
+}
