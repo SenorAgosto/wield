47f8b57965397335f3310bf6711ab8d69837d94a
Austin Gilbert <ceretullis@gmail.com>
2015-04-23 15:15:59 -0500

    Making the example SchedulingPolicy implementations consistent.

--- wield/unit_test/test_raw/Scheduler.hpp
+++ wield/unit_test/test_raw/Scheduler.hpp
@@ -7,17 +7,21 @@
 namespace test_raw {
     
     template<typename Dispatcher, typename PollingPolicy>
-    class SchedulingPolicy final
+    class SchedulingPolicy : public PollingPolicy
     {
     public:
         using StageType = typename Dispatcher::StageType;
         using StageEnumType = typename Dispatcher::StageEnumType;
-        using PollingInformation = typename PollingPolicy::PollingInformation;
-        
+
         SchedulingPolicy(Dispatcher& dispatcher, const std::size_t numberOfThreads = std::thread::hardware_concurrency())
             : dispatcher_(dispatcher)
             , numberOfThreads_(numberOfThreads)
         {
+        }
+
+        inline std::size_t numberOfThreads(void) const
+        {
+            return numberOfThreads_;
         }
 
         typename Dispatcher::StageType& nextStage(const std::size_t /* thread_id */)
@@ -25,33 +29,12 @@ namespace test_raw {
             // this is where the magic happens...
             return dispatcher_[Dispatcher::StageEnumType::Stage1];
         }
-        
-        inline bool continueProcessing(PollingInformation& pollingInfo)
-        {
-            return pollingPolicy_.continueProcessing(pollingInfo);
-        }
-        
-        inline void batchStart(PollingInformation& pollingInfo)
-        {
-            pollingPolicy_.batchStart(pollingInfo);
-        }
-        
-        inline void batchEnd(PollingInformation& pollingInfo)
-        {
-            pollingPolicy_.batchEnd(pollingInfo);
-        }
-        
-        inline std::size_t numberOfThreads(void) const
-        {
-            return numberOfThreads_;
-        }
 
     private:
         SchedulingPolicy(const SchedulingPolicy&) = delete;
         SchedulingPolicy& operator=(const SchedulingPolicy&) = delete;
 
     private:
-        PollingPolicy pollingPolicy_;
         Dispatcher& dispatcher_;
         std::size_t numberOfThreads_;
     };
