75b03bc75e01f37e5266bbb9693fcedc11e6c3f4
Austin Gilbert <ceretullis@gmail.com>
2015-05-04 17:20:17 -0500

    Suppress some warnings from TBB & Boost.ASIO.

--- wield/unit_test/platform/UnitTestSupport.hpp
+++ wield/unit_test/platform/UnitTestSupport.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
-#pragma warning(push)
-#pragma warning(disable : 4127 4350)
-#include <UnitTest++/UnitTest++.h>
-#pragma warning(pop)
+#ifdef WIN32
+    #pragma warning(push)
+    #pragma warning(disable : 4127 4350)
+    #include <UnitTest++/UnitTest++.h>
+    #pragma warning(pop)
+#else
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wnewline-eof"
+    #include <UnitTest++/UnitTest++.h>
+    #pragma clang diagnostic pop
+#endif
+
