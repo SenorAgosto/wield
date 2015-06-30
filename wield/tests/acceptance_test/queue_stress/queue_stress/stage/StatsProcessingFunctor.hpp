f3707d16fa29640ceb19037a2382658a94355c19
Austin Gilbert <ceretullis@gmail.com>
2015-06-30 10:11:19 -0500

    Changing queue_stress test to be more useful for benchmarking. Printing the amount of wallclock time spent processing 100 million messages.

--- wield/tests/acceptance_test/queue_stress/queue_stress/stage/StatsProcessingFunctor.hpp
+++ wield/tests/acceptance_test/queue_stress/queue_stress/stage/StatsProcessingFunctor.hpp
@@ -14,37 +14,20 @@ namespace queue_stress { namespace stage {
     public:
         StatsProcessingFunctor()
             : count_(0)
-            , lastTime_(std::chrono::high_resolution_clock::now())
         {
         }
 
         void operator()(message::TestMessage&) override
         {
-            if(count_++ % ReportInterval == 0)
-            {
-                report_stats();
-            }
-        }
-
-    private:
-
-        void report_stats() 
-        {
-            TimePoint now = std::chrono::high_resolution_clock::now();
-
-            std::chrono::high_resolution_clock::duration duration = now - lastTime_;
-            lastTime_ = now;
-
-            double milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-            auto messageRate = (static_cast<double>(ReportInterval) / milliseconds) * 1000;
-
-            std::cout << messageRate << " msg/s" << std::endl;
+            // if we don't do any logic here,
+            // we actually run slower because
+            // of the effect of cache-line ping-pong
+            // on the message reference count.
+            count_++;
         }
 
     private:
         static const std::size_t ReportInterval = 1000000;
         std::size_t count_;
-
-        TimePoint lastTime_;
     };
 }}
