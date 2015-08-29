d5125870922f545b90cc6e5f32e13043e1ce6a2b
Austin Gilbert <ceretullis@gmail.com>
2015-08-29 18:14:18 -0500

    Consolidate multiple post build events into one with multiple commands.

--- _cmake/post_build/VLD_setup.cmake
+++ _cmake/post_build/VLD_setup.cmake
@@ -3,30 +3,11 @@ if(WIN32)
 	add_custom_command(
 		TARGET wield-UT 
 		POST_BUILD
-		COMMAND @echo off)
-
-	add_custom_command(
-		TARGET wield-UT
-		POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/configs/vld.ini ${CMAKE_CURRENT_BINARY_DIR})
-
-	add_custom_command(
-		TARGET wield-UT
-		POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/configs/ ${CMAKE_CURRENT_BINARY_DIR}/configs/ )
-
-	add_custom_command(
-		TARGET wield-UT
-		POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy "\\lib\\x64\\vc110\\VLD\\2.2.3\\lib\\debug\\vld_x64.dll" ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR})
-
-	add_custom_command(
-		TARGET wield-UT
-		POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy "\\lib\\x64\\vc110\\VLD\\2.2.3\\lib\\debug\\dbghelp.dll" ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR})
-
-	add_custom_command(
-		TARGET wield-UT
-		POST_BUILD
-		COMMAND ${CMAKE_COMMAND} -E copy "\\lib\\x64\\vc110\\VLD\\2.2.3\\lib\\debug\\Microsoft.DTfW.DHL.manifest" ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR})
+		COMMAND @echo off
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/configs/vld.ini ${CMAKE_CURRENT_BINARY_DIR}
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/configs/ ${CMAKE_CURRENT_BINARY_DIR}/configs/ 
+		COMMAND ${CMAKE_COMMAND} -E copy "\\lib\\x64\\vc110\\VLD\\2.2.3\\lib\\debug\\vld_x64.dll" ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}
+		COMMAND ${CMAKE_COMMAND} -E copy "\\lib\\x64\\vc110\\VLD\\2.2.3\\lib\\debug\\dbghelp.dll" ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}
+		COMMAND ${CMAKE_COMMAND} -E copy "\\lib\\x64\\vc110\\VLD\\2.2.3\\lib\\debug\\Microsoft.DTfW.DHL.manifest" ${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_CFG_INTDIR}
+	)
 endif()
