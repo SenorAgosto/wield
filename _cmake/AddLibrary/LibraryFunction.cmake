2d26bebea45b74e3714a3ee9d3abeef10ca8f922
Austin Gilbert <ceretullis@gmail.com>
2015-08-29 20:43:38 -0500

    Updating to lastest version of AddLibrary. Has support for library installation.

--- _cmake/AddLibrary/LibraryFunction.cmake
+++ _cmake/AddLibrary/LibraryFunction.cmake
@@ -1,9 +1,16 @@
 function(MAKE_LIBRARY LIB_NAME)
 	set(library_name "${LIB_NAME}")
 	
-	cmake_parse_arguments(PARSED_ARGS "" "NAME" "DEPENDENCIES;UNITTEST_LIBS" ${ARGN})
+	cmake_parse_arguments(PARSED_ARGS "" "NAME" "DEPENDENCIES;UNITTEST_LIBS;GENERATED_SOURCE_FILES;GENERATED_TESTING_SOURCE_FILES;GENERATED_UT_SOURCE_FILES;COMPILE_FLAGS;TESTING_COMPILE_FLAGS;UT_COMPILE_FLAGS" ${ARGN})
 	set(dependencies ${PARSED_ARGS_DEPENDENCIES})
 	set(unit_test_dependencies ${PARSED_ARGS_UNITTEST_LIBS})
+	set(generated_source_files ${PARSED_ARGS_GENERATED_SOURCE_FILES})
+	set(generated_testing_source_files ${PARSED_ARGS_GENERATED_TESTING_SOURCE_FILES})
+	set(generated_ut_source_files ${PARSED_ARGS_GENERATED_UT_SOURCE_FILES})
+	
+	set(compiler_flags ${PARSED_ARGS_COMPILE_FLAGS})
+	set(testing_compiler_flags ${PARSED_ARGS_TESTING_COMPILE_FLAGS})
+	set(ut_compiler_flags ${PARSED_ARGS_UT_COMPILE_FLAGS})
 
 	message("Adding Library '${library_name}'")
 	
@@ -11,7 +18,11 @@ function(MAKE_LIBRARY LIB_NAME)
 		${CMAKE_CURRENT_SOURCE_DIR} 
 		source_files 
 		FILTER_DIRS "tests" "testing")
-	add_library(${library_name} STATIC ${source_files})
+	add_library(${library_name} STATIC ${source_files} ${generated_source_files})
+
+	foreach(flag ${compiler_flags})
+		set_property(TARGET ${library_name} APPEND_STRING PROPERTY COMPILE_FLAGS "${flag} ")
+	endforeach(flag)
 
 	if(${dependencies})
 		add_dependencies(${library_name} ${dependencies})
@@ -23,19 +34,28 @@ function(MAKE_LIBRARY LIB_NAME)
 		set(testing_lib "${library_name}-Testing")	
 		message("Adding Library '${testing_lib}'")
 
-		add_source("${CMAKE_CURRENT_SOURCE_DIR}/testing/" testing_source)
-		add_library(${testing_lib} ${testing_source})
+		add_source("${CMAKE_CURRENT_SOURCE_DIR}/testing/" testing_source NO_INSTALL true)
+		add_library(${testing_lib} ${testing_source} ${generated_testing_source_files})
+
+		foreach(flag ${testing_compiler_flags})
+			set_property(TARGET ${testing_lib} APPEND_STRING PROPERTY COMPILE_FLAGS "${flag} ")
+		endforeach(flag)
 
 		add_dependencies(${testing_lib} ${library_name})
 	endif()
+
 
 	# build a unit test executable
 	if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/")
 		set(executable_name "${library_name}-UT")
 		message("Adding Unit Test Executable '${executable_name}'")
 
-		add_source("${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/" ut_implementation)
-		add_executable(${executable_name} ${ut_implementation})
+		add_source("${CMAKE_CURRENT_SOURCE_DIR}/tests/unit_test/" ut_implementation NO_INSTALL true)
+		add_executable(${executable_name} ${ut_implementation} ${generated_ut_source_files})
+
+		foreach(flag ${ut_compiler_flags})
+			set_property(TARGET ${executable_name} APPEND_STRING PROPERTY COMPILE_FLAGS "${flag} ")
+		endforeach(flag)
 
 		add_dependencies(
 			${executable_name}
@@ -48,7 +68,9 @@ function(MAKE_LIBRARY LIB_NAME)
 			 ${library_name}
 			 ${testing_lib}
 			 ${dependencies}
+			 ${platform_unit_test_lib}
 			 ${unit_test_dependencies}
+			 ${platform_dependencies}
 		)
 
 		# run the UT as a post build event
