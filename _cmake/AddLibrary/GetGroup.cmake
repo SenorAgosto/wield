7a4e5ea3528b56a76061b7fef7b588118a5ba669
Austin Gilbert <ceretullis@gmail.com>
2015-09-15 16:48:05 -0500

    Updating AddLibrary to latest.

--- _cmake/AddLibrary/GetGroup.cmake
+++ _cmake/AddLibrary/GetGroup.cmake
@@ -4,14 +4,18 @@ function(get_group path default_group group)
 	get_filename_component(dir ${path} DIRECTORY)
 
 	if(dir)
-		string(REGEX REPLACE "(^src$|^src/)" "" dir ${dir})	# special handling for src/ in libs.
+		string(REGEX MATCH "(^src$|^src/)" matched ${dir})	# special handling for src/ in libs.
 
-		if(dir)
-			string(REGEX REPLACE "/" "\\\\" dir ${dir})
-			set(${group} "${default_group}\\${dir}" PARENT_SCOPE)
-		else()
-			set(${group} ${default_group} PARENT_SCOPE)
-		endif()
+        if(matched)
+            string(REGEX REPLACE "^src$" "source" dir ${dir})
+            string(REGEX REPLACE "^src/" "source/" dir ${dir})
+            string(REGEX REPLACE "/" "\\\\" dir ${dir})
+
+            set(${group} "${dir}" PARENT_SCOPE)
+        else()
+            string(REGEX REPLACE "/" "\\\\" dir ${dir})
+            set(${group} "${default_group}\\${dir}" PARENT_SCOPE)
+        endif()
 	else()
 		set(${group} ${default_group} PARENT_SCOPE)
 	endif()
