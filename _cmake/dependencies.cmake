e7463409beb3d4644087978f8ec8a1e3b459b70a
Austin Gilbert <ceretullis@gmail.com>
2015-10-07 20:52:48 -0500

    CompilerFlags was removed in earlier commit.

--- _cmake/dependencies.cmake
+++ _cmake/dependencies.cmake
@@ -1,6 +1,3 @@
-# project configuration 
-include(_cmake/CompilerFlags.cmake)
-
 # include add our project to include path
 include_directories(${CMAKE_SOURCE_DIR})
 include(_cmake/AddLibrary/Dependencies.cmake)
