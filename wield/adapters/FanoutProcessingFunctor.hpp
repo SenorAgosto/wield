93d1bebe08f9878533eb554d4d62f80371c676db
Austin Gilbert <austin.gilbert@gmail.com>
2016-10-04 23:03:52 -0500

    Implementing a utility adapter to assist client code in doing message fanout after processing a message. The helper class FanoutProcessingFunctor is an abstract base class which client code inherits from; client code then implements processing methods to meet its ProcessingFunctorInterface and uses the base class's implementation of dispatch() instead of Dispatcher::dispatch().
    See wield/tests/unit_test/testFanoutProcessingFunctor-UT.cpp for an example use case.

---
+++ wield/adapters/FanoutProcessingFunctor.hpp
@@ -0,0 +1,87 @@
+#pragma once
+#include <wield/CloneMessageTag.hpp>
+#include <utility>
+#include <vector>
+
+namespace wield { namespace adapters {
+    
+    // An abstract base class which implements logic necessary to
+    // fanout a message to multiple downstream stages. The stages
+    // to fanout to determined at construction.
+    template<class Dispatcher, class ProcessingFunctorInterface>
+    class FanoutProcessingFunctor : public ProcessingFunctorInterface
+    {
+    public:
+        using StageEnumType = typename Dispatcher::StageEnumType;
+        using Message = typename ProcessingFunctorInterface::Message;
+        
+        FanoutProcessingFunctor(Dispatcher& dispatcher, const std::vector<StageEnumType>& stages);
+        FanoutProcessingFunctor(Dispatcher& dispatcher, std::vector<StageEnumType>&& stages);
+        
+        // NOTE: use this dispatch() method instead of Dispatcher::dispatch() in
+        // your concrete processing functor.
+        void dispatch(Message& m);
+        
+        // NOTE: use this dispatch() method instead of Dispatcher::dispatch() in
+        // your concrete processing functor.
+        template<class ConcreteMessageType>
+        void dispatch(ConcreteMessageType& message, CloneMessageTagType);
+        
+        // swap the current fanout configuration with @stages
+        void updateStages(const std::vector<StageEnumType>& stages);
+        void updateStages(std::vector<StageEnumType>&& stages);
+        
+    private:
+        std::vector<StageEnumType> stages_;
+        Dispatcher& dispatcher_;
+    };
+    
+    
+    template<class Dispatcher, class ProcessingFunctorInterface>
+    FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>::FanoutProcessingFunctor(Dispatcher& dispatcher, const std::vector<StageEnumType>& stages)
+        : stages_(stages)
+        , dispatcher_(dispatcher)
+    {
+    }
+    
+    template<class Dispatcher, class ProcessingFunctorInterface>
+    FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>::FanoutProcessingFunctor(Dispatcher& dispatcher, std::vector<StageEnumType>&& stages)
+        : stages_(std::move(stages))
+        , dispatcher_(dispatcher)
+    {
+    }
+    
+    template<class Dispatcher, class ProcessingFunctorInterface>
+    inline
+    void FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>::dispatch(Message& m)
+    {
+        std::for_each(begin(stages_), end(stages_), [this, &m](const StageEnumType stage){
+            this->dispatcher_.dispatch(stage, m);
+        });
+    }
+    
+    template<class Dispatcher, class ProcessingFunctorInterface>
+    template<class ConcreteMessageType>
+    inline
+    void FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>::dispatch(ConcreteMessageType& m, CloneMessageTagType)
+    {
+        std::for_each(begin(stages_), end(stages_), [this, &m](const StageEnumType stage){
+            this->dispatcher_.dispatch(stage, m, clone_message);
+        });
+    }
+    
+    template<class Dispatcher, class ProcessingFunctorInterface>
+    inline
+    void FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>::updateStages(const std::vector<StageEnumType>& stages)
+    {
+        stages_ = stages;
+    }
+    
+    template<class Dispatcher, class ProcessingFunctorInterface>
+    inline
+    void FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>::updateStages(std::vector<StageEnumType>&& stages)
+    {
+        stages_.swap(stages);
+    }
+}}
+
---
+++ wield/tests/unit_test/testFanoutProcessingFunctor-UT.cpp
@@ -0,0 +1,147 @@
+#include "./platform/UnitTestSupport.hpp"
+
+#include <wield/adapters/FanoutProcessingFunctor.hpp>
+#include <wield/CloneMessageTag.hpp>
+
+#include "./test/Traits.hpp"
+#include "./test/ProcessingFunctor.hpp"
+#include "./test/Message.hpp"
+
+namespace {
+    
+    using namespace test;
+    using Dispatcher = Traits::Dispatcher;
+    using Queue = Traits::Queue;
+    using Stage = Traits::Stage;
+    using Message = Traits::Message;
+    
+    template<class Dispatcher>
+    class FanoutProcessingFunctor : public wield::adapters::FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>
+    {
+    public:
+        using wield::adapters::FanoutProcessingFunctor<Dispatcher, ProcessingFunctorInterface>::FanoutProcessingFunctor;
+        
+        void operator() (Message& m) override
+        {
+            this->dispatch(m);
+        }
+        
+        void operator()(TestMessage& m) override
+        {
+            this->dispatch(m);
+        }
+        
+        void operator()(TestMessage2& m) override
+        {
+            this->dispatch(m, wield::clone_message);
+        }
+    };
+    
+    struct FanoutProcessingFunctorFixture
+    {
+        FanoutProcessingFunctorFixture()
+            : fanout(d, {{Stages::Stage2, Stages::Stage3}})
+            , s1(Stages::Stage1, d, q1, fanout)
+            , s2(Stages::Stage2, d, q2, pf2)
+            , s3(Stages::Stage3, d, q3, pf3)
+        {
+        }
+        
+        Dispatcher d;
+
+        Queue q1;
+        FanoutProcessingFunctor<Dispatcher> fanout;
+        Stage s1;
+        
+        Queue q2;
+        ProcessingFunctor pf2;
+        Stage s2;
+        
+        Queue q3;
+        ProcessingFunctor pf3;
+        Stage s3;
+    };
+    
+    TEST_FIXTURE(FanoutProcessingFunctorFixture, verifyInstantiation)
+    {
+    }
+    
+    TEST_FIXTURE(FanoutProcessingFunctorFixture, verifyFanoutOnProcessing)
+    {
+        Message::smartptr m = new TestMessage();
+        d.dispatch(Stages::Stage1, *m);
+        
+        CHECK(s1.process());   // this should cause the message to be dispatched to s2 & s3.
+        
+        CHECK(s2.process());
+        CHECK(s3.process());
+        
+        CHECK(pf2.message1Called_); // ensure we processed a TestMessage
+        CHECK(pf3.message1Called_); // ensure we processed a TestMessage
+    }
+
+    TEST_FIXTURE(FanoutProcessingFunctorFixture, verifyFanoutConfigurationCanBeChanged)
+    {
+        Message::smartptr m = new TestMessage2();
+        d.dispatch(Stages::Stage1, *m);
+        
+        CHECK(s1.process());    // dispatch m to s2 & s3
+        CHECK(s2.process());
+        CHECK(s3.process());
+        
+        CHECK_EQUAL(1U, pf2.message2CallCount_);
+        CHECK_EQUAL(1U, pf3.message2CallCount_);
+        
+        const std::vector<Stages> stages = {Stages::Stage3};
+        fanout.updateStages(stages);
+        
+        // send m to s1 again
+        d.dispatch(Stages::Stage1, *m);
+        
+        CHECK(s1.process());    // processing now dispatches only to s3
+        CHECK(!s2.process());   // s2 should not get m this time
+        CHECK(s3.process());
+        
+        CHECK_EQUAL(1U, pf2.message2CallCount_);
+        CHECK_EQUAL(2U, pf3.message2CallCount_);
+    }
+
+    TEST_FIXTURE(FanoutProcessingFunctorFixture, verifyFanoutConfigurationCanBeChangedWithRvalueReference)
+    {
+        Message::smartptr m = new TestMessage();
+        d.dispatch(Stages::Stage1, *m);
+        
+        CHECK(s1.process());    // dispatch m to s2 & s3
+        CHECK(s2.process());
+        CHECK(s3.process());
+        
+        CHECK_EQUAL(1U, pf2.message1CallCount_);
+        CHECK_EQUAL(1U, pf3.message1CallCount_);
+        
+        fanout.updateStages({Stages::Stage3});
+        
+        // send m to s1 again
+        d.dispatch(Stages::Stage1, *m);
+        
+        CHECK(s1.process());    // processing now dispatches only to s3
+        CHECK(!s2.process());   // s2 should not get m this time
+        CHECK(s3.process());
+        
+        CHECK_EQUAL(1U, pf2.message1CallCount_);
+        CHECK_EQUAL(2U, pf3.message1CallCount_);
+    }
+    
+    TEST_FIXTURE(FanoutProcessingFunctorFixture, verifyFanoutOnProcessingWithClonedMessage)
+    {
+        Message::smartptr m = new TestMessage2();
+        d.dispatch(Stages::Stage1, *m);
+        
+        CHECK(s1.process());   // this should cause the message to be dispatched to s2 & s3.
+        
+        CHECK(s2.process());
+        CHECK(s3.process());
+        
+        CHECK(pf2.message2Called_); // ensure we processed a TestMessage
+        CHECK(pf3.message2Called_); // ensure we processed a TestMessage
+    }
+}
